b7990a106a8c2cd0d4632c411715fac914592c89
Jeremy Banks <j@jeremybanks.ca>
2015-12-13 22:02:19 -0800

    m

--- stockfighter/trades-exec/index.js
+++ stockfighter/trades-exec/index.js
@@ -9,4 +9,75 @@ class Client {
   }
 }
 
+
+class Level {
+  constructor(client, name) {
+    this.client = client;
+    this.name = name;
+  }
+}
+
+
+class Instance {
+  constructor(level, id) {
+    this.client = client;
+    this.id = id;
+  }
+}
+
+
+class Stock {
+  constructor(instance, symbol) {
+    this.instance = instance;
+    this.symbol = symbol;
+  }
+}
+
+
+class Venue {
+  constructor(instance, name) {
+    this.instance = instance;
+    this.name = name;
+  }
+}
+
+
+class Ticker {
+  constructor(venue, stock) {
+    this.venue = venue;
+    this.stock = stock;
+  }
+}
+
+
+class OwnOrder {
+  constructor(ticker, id) {
+    this.ticker = ticker;
+    this.id = id;
+  }
+}
+
+
+class AnOrder {
+  constructor(ticker) {
+    this.ticker = ticker;
+    // TODO: all properties should be required here.
+  }
+}
+
+
+class Quote {
+  constructor(ticker) {
+    this.ticker = ticker;
+  }
+}
+
+
+class Execution {
+  constructor(ticker) {
+    this.ticker = ticker;
+  }
+}
+
+
 module.exports = {Client};
